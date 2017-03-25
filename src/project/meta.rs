48082b3c5c7dc36114363d567a78c73165987d58
Hunar Roop Kahlon <hunar.roop@gmail.com>
2017-03-25 10:44:39 -0700

    Add docs
    Signed-off-by: Hunar Roop Kahlon <hunar.roop@gmail.com>

--- src/project/meta.rs
+++ src/project/meta.rs
@@ -1,5 +1,8 @@
-use std::fmt::{ Debug, Display, Formatter, Result };
+use std::fmt::{Debug, Display, Formatter, Result};
 
+/// Specifies the category of a project.
+///
+/// Every project can only have one `Category`
 #[derive(Clone, Copy)]
 pub enum Category {
     AdminTools,
@@ -12,19 +15,21 @@ pub enum Category {
     RolePlaying,
     WorldManagement,
     Miscellaneous,
-    Undefined
+    Undefined,
 }
 
+/// Specifies the sort query type.
 #[derive(Clone, Copy)]
 pub enum SortType {
     MostStars,
     MostDownloads,
     MostViews,
     Newest,
-    RecentlyUpdated
+    RecentlyUpdated,
 }
 
 impl Category {
+    /// Gets a `Category` from a string.
     pub fn from_string(name: &str) -> Self {
         match name {
             "AdminTools" => Category::AdminTools,
@@ -37,10 +42,11 @@ impl Category {
             "RolePlaying" => Category::RolePlaying,
             "WorldManagement" => Category::WorldManagement,
             "Miscellaneous" => Category::Miscellaneous,
-            _ => Category::Undefined
+            _ => Category::Undefined,
         }
     }
 
+    /// Gets a `Category` from an id.
     pub fn from_id(id: &u8) -> Self {
         match id {
             &0u8 => Category::AdminTools,
@@ -53,10 +59,11 @@ impl Category {
             &7u8 => Category::RolePlaying,
             &8u8 => Category::WorldManagement,
             &9u8 => Category::Miscellaneous,
-            _ => Category::Undefined
+            _ => Category::Undefined,
         }
     }
 
+    /// Converts a `Category` to its corresponding id.
     pub fn to_id(&self) -> u8 {
         match *self {
             Category::AdminTools => 0u8,
@@ -69,30 +76,34 @@ impl Category {
             Category::RolePlaying => 7u8,
             Category::WorldManagement => 8u8,
             Category::Miscellaneous => 9u8,
-            _ => 10u8
+            _ => 10u8,
         }
     }
 
     fn as_string(&self) -> String {
         match *self {
-            Category::AdminTools => "AdminTools",
-            Category::Chat => "Chat",
-            Category::DeveloperTools => "DeveloperTools",
-            Category::Economy => "Economy",
-            Category::Gameplay => "Gameplay",
-            Category::Games => "Games",
-            Category::Protection => "Protection",
-            Category::RolePlaying => "RolePlaying",
-            Category::WorldManagement => "WorldManagement",
-            Category::Miscellaneous => "Miscellaneous",
-            _ => "Undefined"
-        }.to_string()
+                Category::AdminTools => "AdminTools",
+                Category::Chat => "Chat",
+                Category::DeveloperTools => "DeveloperTools",
+                Category::Economy => "Economy",
+                Category::Gameplay => "Gameplay",
+                Category::Games => "Games",
+                Category::Protection => "Protection",
+                Category::RolePlaying => "RolePlaying",
+                Category::WorldManagement => "WorldManagement",
+                Category::Miscellaneous => "Miscellaneous",
+                _ => "Undefined",
+            }
+            .to_string()
     }
 }
 
 impl Debug for Category {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "Category {{ id: {}, name: {} }}", self.to_id(), self.as_string())
+        write!(f,
+               "Category {{ id: {}, name: {} }}",
+               self.to_id(),
+               self.as_string())
     }
 }
 
@@ -103,50 +114,57 @@ impl Display for Category {
 }
 
 impl SortType {
+    /// Gets a `SortType` from a string.
     pub fn from_string(name: &str) -> Self {
         match name {
             "MostDownloads" => SortType::MostDownloads,
             "MostViews" => SortType::MostViews,
             "Newest" => SortType::Newest,
             "RecentlyUpdated" => SortType::RecentlyUpdated,
-            _ => SortType::MostStars
+            _ => SortType::MostStars,
         }
     }
 
+    /// Gets a `SortType` from an id.
     pub fn from_id(id: &u8) -> Self {
         match id {
             &1u8 => SortType::MostDownloads,
             &2u8 => SortType::MostViews,
             &3u8 => SortType::Newest,
             &4u8 => SortType::RecentlyUpdated,
-            _ => SortType::MostStars
+            _ => SortType::MostStars,
         }
     }
 
+    /// Converts a `SortType` to its corresponding id.
     pub fn to_id(&self) -> u8 {
         match *self {
             SortType::MostStars => 0u8,
             SortType::MostDownloads => 1u8,
             SortType::MostViews => 2u8,
             SortType::Newest => 3u8,
-            SortType::RecentlyUpdated => 4u8
+            SortType::RecentlyUpdated => 4u8,
         }
     }
 
     fn as_string(&self) -> String {
         match *self {
-            SortType::MostStars => "MostStars",
-            SortType::MostDownloads => "MostDownloads",
-            SortType::MostViews => "MostViews",
-            SortType::Newest => "Newest",
-            SortType::RecentlyUpdated => "RecentlyUpdated"
-        }.to_string()
+                SortType::MostStars => "MostStars",
+                SortType::MostDownloads => "MostDownloads",
+                SortType::MostViews => "MostViews",
+                SortType::Newest => "Newest",
+                SortType::RecentlyUpdated => "RecentlyUpdated",
+            }
+            .to_string()
     }
 }
 
 impl Debug for SortType {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "SortType {{ id: {}, name: {} }}", self.to_id(), self.as_string())
+        write!(f,
+               "SortType {{ id: {}, name: {} }}",
+               self.to_id(),
+               self.as_string())
     }
 }
 
--- src/project/mod.rs
+++ src/project/mod.rs
@@ -1,1 +1,3 @@
+/// Provides the meta for Project query.
+// TODO rename to query_meta
 pub mod meta;
